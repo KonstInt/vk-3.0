026796b486cff898397e4bb8c6164895faac3305
KonstInt <58230320+KonstInt@users.noreply.github.com>
2020-01-08 10:47:11 +0300

    default

---
+++ app/src/main/java/com/ovs_corp/intrf/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ovs_corp.intrf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
